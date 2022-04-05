f809006685be3c102086ed055bcbfb14a721ebf9
TDenisaM <99595805+TDenisaM@users.noreply.github.com>
2022-04-05 23:11:57 +0200

    updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,23 +5,39 @@ import "./Media.css";
 export default function Footer() {
   return (
     <footer className="Footer">
-      <a
-        href="https://github.com/TDenisaM/react-weather-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Open-source on GitHub
-      </a>
       <span>
         {" "}
-        coded by{" "}
+        Coded by{" "}
         <a
           href="https://mellifluous-jelly-540b2e.netlify.app"
           target="_blank"
           rel="noreferrer"
         >
-          Denisa M. Tudor
+          Denisa M. Tudor{" "}
         </a>
+      </span>
+      <span>
+        (open-source on
+        <a
+          href="https://github.com/TDenisaM/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          GitHub{" "}
+        </a>
+      </span>
+      <span>
+        and hosted on
+        <a
+          href="https://fantastic-speculoos-c5d2bc.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Netlify
+        </a>
+        )
       </span>
     </footer>
   );
